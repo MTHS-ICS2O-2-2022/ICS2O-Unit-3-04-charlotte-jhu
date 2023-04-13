74f83754d9388776b03b8a1703514f12eaa0c861
charlotte-jhu <124389290+charlotte-jhu@users.noreply.github.com>
2023-04-13 17:24:18 +0000

    added extra

---
+++ go_app/main.go
@@ -0,0 +1,28 @@
+// Created by: Charlotte Jhu
+// Created on: April 2023
+//
+// This program calculates degrees Fahrenheit to degrees Celsius
+
+package main
+
+import "fmt"
+
+func main() {
+	// This function calculates degrees Fahrenheit to degrees Celsius
+	// variables
+	var fahrenheit float64
+
+	// input
+	fmt.Println("This program calculates degrees Fahrenheit to degrees Celsius")
+	fmt.Println()
+	fmt.Print("Enter the temperature in degrees Fahrenheit: ")
+	fmt.Scanln(&fahrenheit)
+	fmt.Println()
+
+	// process
+	celsius := (fahrenheit - 32) * 5 / 9
+
+	// output
+	fmt.Println("The temperature in Celsius is",celsius)
+	fmt.Println("\nDone.")
+}
