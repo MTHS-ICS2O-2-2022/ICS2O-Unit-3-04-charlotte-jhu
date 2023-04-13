2ceb9c85e9ca9884377fffdbda54680c67052ffd
charlotte-jhu <124389290+charlotte-jhu@users.noreply.github.com>
2023-04-13 17:14:13 +0000

    added website

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Charlotte Jhu All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Charlotte Jhu
+// Created on: April 2023
 // This file contains the JS functions for index.html
+
+'use strict'
+
+/**
+ * This function calculates degrees Fahrenheit to degrees Celsius
+ */
+
+function myButtonClicked() {
+  // input
+  const fahrenheit = parseFloat(document.getElementById('fahrenheit').value)
+
+  // process
+  const celsius = (fahrenheit - 32) * 5 / 9
+
+  // output
+  document.getElementById('celsius').innerHTML = "Tempurature is " + celsius + "°C"
+}
